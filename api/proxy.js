40a2ec320c8770d3d319f48fa630c4fe5deed373
gomescnb <C0012240@cenibra.com.br>
2024-07-16 09:59:46 -0300

    more tests

--- api/proxy.js
+++ api/proxy.js
@@ -45,40 +45,40 @@ app.get("/proxy", async (req, res) => {
 
 app.get("/table/:dado", async (req, res) => {
   try {
-    res.send("teste");
-  // const dado = req.params.dado;
-  // const tableResponse = await axios.get(`https://portalbrasil.net/${dado}`);
-  // console.log(tableResponse.data);
-  // console.log(dado);
-  // let startIndex = "";
-  // let tableParcial = "";
-  // let endIndex = "";
-  // let tableFinal = "";
-  // switch (dado) {
-  //   case "ipca":
-  //     startIndex = tableResponse.data.indexOf("<table");
-  //     endIndex = tableResponse.data.indexOf("</table>");
-  //     tableFinal = tableResponse.data.substring(startIndex, endIndex + 8); // 8 is the length of "</table>" in the original
-  //     return res.send(tableFinal);
-  //   case "igpm":
-  //     startIndex = tableResponse.data.indexOf("<table");
-  //     endIndex = tableResponse.data.indexOf("</table>");
-  //     tableFinal = tableResponse.data.substring(startIndex, endIndex + 8); // 8 is the length of "</table>" in the original
-  //     return res.send(tableFinal);    
-  //   case "inpc":
-  //     startIndex = tableResponse.data.indexOf(
-  //       '<table id="tb" cellspacing="1" cellpadding="3">'
-  //     );
-  //     tableParcial = tableResponse.data.substring(
-  //       startIndex,
-  //       tableResponse.data.length
-  //     );
-  //     endIndex = tableParcial.indexOf("</table>");
-  //     tableFinal = tableParcial.substring(0, endIndex + 8); // 8 is the length of "</table>" in the original
-  //     return res.send(tableFinal);
-  //   default:
-  //     return res.status(404).send("Rota não encontrada");
-  // }
+  const dado = req.params.dado;
+  const tableResponse = await axios.get(`https://portalbrasil.net/${dado}`);
+  console.log(tableResponse.data);
+  console.log(dado);
+  let startIndex = "";
+  let tableParcial = "";
+  let endIndex = "";
+  let tableFinal = "";
+  switch (dado) {
+    case "ipca":
+      startIndex = tableResponse.data.indexOf("<table");
+      endIndex = tableResponse.data.indexOf("</table>");
+      tableFinal = tableResponse.data.substring(startIndex, endIndex + 8); // 8 is the length of "</table>" in the original
+      console.log(tableFinal);
+      return res.send(tableFinal);
+    case "igpm":
+      startIndex = tableResponse.data.indexOf("<table");
+      endIndex = tableResponse.data.indexOf("</table>");
+      tableFinal = tableResponse.data.substring(startIndex, endIndex + 8); // 8 is the length of "</table>" in the original
+      return res.send(tableFinal);    
+    case "inpc":
+      startIndex = tableResponse.data.indexOf(
+        '<table id="tb" cellspacing="1" cellpadding="3">'
+      );
+      tableParcial = tableResponse.data.substring(
+        startIndex,
+        tableResponse.data.length
+      );
+      endIndex = tableParcial.indexOf("</table>");
+      tableFinal = tableParcial.substring(0, endIndex + 8); // 8 is the length of "</table>" in the original
+      return res.send(tableFinal);
+    default:
+      return res.status(404).send("Rota não encontrada");
+  }
 
   } catch (error) {
     console.error("Error fetching data:", error);
